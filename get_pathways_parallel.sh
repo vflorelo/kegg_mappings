5187a77e2e9a1eadd31a0bb682453b7f3ee5ddbf
vflorelo <vflorelo@gmail.com>
2020-06-16 16:53:29 -0500

    fixed org lists

--- get_pathways_parallel.sh
+++ get_pathways_parallel.sh
@@ -2,9 +2,9 @@
 #############################################################################################################################################################
 data_sheet=$(cat "$1" | grep -v "\#")                                                                                                                       #
 annotation_file_list=$(echo "$data_sheet" | awk 'BEGIN{FS="\t"}{if($1=="annotation file"){print $2}}' | perl -pe 's/\,/\n/g' | grep -v ^$ | sort -V | uniq) #
-exclude_org_list=$(echo     "$data_sheet" | awk 'BEGIN{FS="\t"}{if($1=="org exclude"){print $2}}' | perl -pe 's/\,/\n/g' | grep -v ^$ | sort -V | uniq)     #
-include_org_list=$(echo     "$data_sheet" | awk 'BEGIN{FS="\t"}{if($1=="org include"){print $2}}' | perl -pe 's/\,/\n/g' | grep -v ^$ | sort -V | uniq)     #
-kegg_data_dir=$(echo        "$data_sheet" | awk 'BEGIN{FS="\t"}{if($1=="KEGG data dir"){print $2}}' | grep -v ^$ | sort -V | uniq)                          #
+exclude_org_list=$(echo     "$data_sheet" | awk 'BEGIN{FS="\t"}{if($1=="org exclude"){print $2}}'     | perl -pe 's/\,/\n/g' | grep -v ^$ | sort -V | uniq) #
+include_org_list=$(echo     "$data_sheet" | awk 'BEGIN{FS="\t"}{if($1=="org include"){print $2}}'     | perl -pe 's/\,/\n/g' | grep -v ^$ | sort -V | uniq) #
+kegg_data_dir=$(echo        "$data_sheet" | awk 'BEGIN{FS="\t"}{if($1=="KEGG data dir"){print $2}}'   | grep -v ^$ | sort -V | uniq)                        #
 kegg_tmp_dir="$HOME/.kegg_tmp"                                                                                                                              #
 work_dir=$(pwd)                                                                                                                                             #
 file_list=$(echo -e "ortholog_gene_mappings\norg_list\npathway_list\ngene_pathway_mappings")                                                                #
@@ -18,13 +18,6 @@ else                             #
 	kegg_data_dir=${kegg_data_dir} #
 fi                               #
 ##################################
-
-##########################################
-if [ -z "$exclude_org_list" ]            #
-then                                     #
-	exclude_org_list=$(echo -e "hsa\nmmu") #
-fi                                       #
-##########################################
 
 ##########################################################
 missing_files="0"                                        #
@@ -84,12 +77,19 @@ do                                    #
 done                                  #
 #######################################
 
+##########################################
+if [ -z "$exclude_org_list" ]            #
+then                                     #
+	exclude_org_list=$(echo -e "hsa\nmmu") #
+fi                                       #
+##########################################
+
 ##############################################################################################################################
 if [ -z "$include_org_list" ]                                                                                                #
 then                                                                                                                         #
 	include_org_list=$(zcat ${kegg_data_dir}/org_list.gz | cut -f2 | sort -V | uniq | grep -wvFf <(echo "$exclude_org_list") ) #
 else                                                                                                                         #
-	include_org_list="$include_org_list"                                                                                       #
+	include_org_list=$(echo "$include_org_list" | grep -wvFf "$exclude_org_list")                                              #
 fi                                                                                                                           #
 ##############################################################################################################################
 
@@ -98,9 +98,9 @@ cd ${work_dir}                                                                  
 for annotation_file in $annotation_file_list                                                                                                                                        #
 do                                                                                                                                                                                  #
 	kegg_term_list=$(cut -f2 ${annotation_file} | perl -pe 's/\,/\n/g' | grep -v ^$ | cut -f2 -d\: | sort -V | uniq)                                                                  #
-  ortholog_list=$(zgrep -wFf <(echo "$kegg_term_list") ${kegg_data_dir}/ortholog_gene_mappings.gz | grep -wFf <(echo "$include_org_list") | cut -f2 | cut -d: -f2 | sort -V | uniq) #
+  ortholog_list=$(zgrep -wFf <(echo "$kegg_term_list") ${kegg_data_dir}/ortholog_gene_mappings.gz | grep -wFf <(echo "$include_org_list") | grep -wvFf <(echo "$exclude_org_list") | cut -f2 | cut -d: -f2 | sort -V | uniq)#
   pathway_list=$(zgrep -wFf <(echo "$ortholog_list") ${kegg_data_dir}/gene_pathway_mappings.gz | cut -f2 | cut -d\: -f2 | sort -V | uniq )                                          #
-	zgrep -wFf <(echo "$pathway_list")   ${kegg_data_dir}/pathway_list.gz | perl -pe 's/path\://;s/\t/\{/;s/$/\}/' > ${kegg_tmp_dir}/pathway_names                                               #
+	zgrep -wFf <(echo "$pathway_list")   ${kegg_data_dir}/pathway_list.gz | perl -pe 's/path\://;s/\t/\{/;s/$/\}/' > ${kegg_tmp_dir}/pathway_names                                    #
 	zgrep -wFf <(echo "$kegg_term_list") ${kegg_data_dir}/ortholog_gene_mappings.gz | grep -wFf <(echo "$include_org_list") > ${kegg_tmp_dir}/ortholog_gene_mappings.tsv              #
 	zgrep -wFf <(echo "$ortholog_list")  ${kegg_data_dir}/gene_pathway_mappings.gz > ${kegg_tmp_dir}/gene_pathway_mappings.tsv                                                        #
 	awk 'BEGIN{FS="\t"}{print "get_pathways.sh "$1,$2}' $annotation_file | parallel -j12                                                                                              #
